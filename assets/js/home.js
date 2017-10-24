86d068b5d631c1e112749a2f3123cb0264273437
Taylin Large <taylin.large@gmail.com>
2017-10-23 21:00:55 -0400

    working on mobile

---
+++ assets/js/home.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click',function() {
+	document.querySelector('.hamburger').classList.toggle('is-active');
+	document.querySelector('.hamburger-menu').classList.toggle('active');
+});
