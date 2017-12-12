d7d2b13bc63524d0be7198e83321cfb4f976c8e9
Taylin Large <taylin.large@gmail.com>
2017-12-12 13:06:02 -0500

    woork

--- assets/js/home.js
+++ assets/js/home.js
@@ -12,10 +12,3 @@ $(function() {
 	TweenMax.from('.hero-text h1', 1, {opacity: 0, top: "-20px"});
 	TweenMax.from('.hero-text p', 1, {opacity: 0, delay: 0.7});
 })
-
-// var waypoints = $('.details').waypoint({
-//   handler: function(direction) {
-//     TweenMax.from('.details-text', 1, {display: flex, opacity: 0, left: "-10px"});
-//   },{
-//   	offset: "20%"
-// });
