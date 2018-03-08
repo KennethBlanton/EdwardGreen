18f0e9caa0422f4def8ea3f65b804ad1bebbb48d
Taylin Large <taylin.large@gmail.com>
2018-03-07 21:10:20 -0500

    finished styling

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,10 +3,17 @@ document.querySelector('.hamburger').addEventListener('click',function() {
 	document.querySelector('.hamburger-menu').classList.toggle('active');
 });
 
+window.onresize = function() {
+	if (window.innerWidth >= 765) {
+		document.querySelector('.hamburger-menu').classList.remove('active');
+		document.querySelector('.hamburger').classList.remove('is-active');
+		document.querySelector('.member-menu').classList.remove('active');
+	}	
+}
+
 document.querySelector('.member').addEventListener('click',function() {
 	document.querySelector('.member-menu').classList.toggle('active');
 });
-
 
 $(function() {
 	TweenMax.from('.hero-text h1', 1, {opacity: 0, top: "-20px"});
